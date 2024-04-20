fe83162716ad89ff7a67a7eb89b6a21814707eeb
Nikkita <nikbann@bk.ru>
2024-04-20 18:07:25 +0300

    second

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,25 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const cardPlacement = document.querySelector('.places__list');
 
-function deleteCard(event){
+function createCard(data, onDelete){
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardImg = cardElement.querySelector('.card__image')
+  cardImg.src = data.link;
+  cardImg.alt = data.name;
+  cardElement.querySelector('.card__title').textContent = data.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', onDelete);
+  return cardElement;
+}
+
+function handleDeleteCard(event){
   event.target.closest('.card').remove();
 }
 
-function addCard(card){
-  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardElement.querySelector('.card__image').src = card.link;
-  cardElement.querySelector('.card__title').textContent = card.name;
-  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+function renderCard(cardElement){
   cardPlacement.append(cardElement)
 }
 
-initialCards.forEach(addCard);
+initialCards.forEach((cards) => {
+  renderCard(createCard(cards, handleDeleteCard))
+});
